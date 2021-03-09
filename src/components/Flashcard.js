af6f58a26ae711ca82d374e94266db24b67977c8
techiphyte76 <77113884+techiphyte76@users.noreply.github.com>
2021-03-09 18:30:26 -0500

    Changed app.css to App.css

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import '../app.css';
+import '../App.css';
 
 export default function Flashcard({ flashcard }) {
   const [flip, setFlip] = useState(false);
